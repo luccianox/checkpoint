1e39b64c27225315248f5c252b43517aba46184e
luccianox <kevinnambubbi@gmail.com>
2025-09-23 19:35:56 +0300

    chunk

---
+++ chunk.go
@@ -0,0 +1,44 @@
+/*
+chunk
+Instructions
+Write a function called Chunk that receives as parameters a slice, slice []int, and a number size int.
+The goal of this function is to chunk a slice into many sub slices where each sub slice has the length of size.
+
+If the size is 0 it should print a newline ('\n').
+Expected function
+*/
+package main
+
+import "fmt"
+
+func Chunk(slice []int, size int) {
+
+	if size == 0 {
+		fmt.Println()
+		return
+	}
+	if len(slice) == 0 {
+
+		fmt.Println([][]int{})
+		return
+	}
+	result := [][]int{}
+	//loop thru slice
+
+	for i := 0; i < len(slice); i += size {
+
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
+		}
+		result = append(result, slice[i:end])
+	}
+	fmt.Println(result)
+}
+func main() {
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+}
