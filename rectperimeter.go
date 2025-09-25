458777803db7f6f532c5e67c3f8e7b731317c8ab
luccianox <kevinnambubbi@gmail.com>
2025-09-25 08:43:29 +0300

    rectperimeter

---
+++ rectperimeter.go
@@ -0,0 +1,10 @@
+/*rectperimeter
+Instructions
+Write a function that takes two int's as arguments, representing the
+length of width and height of a rectangle and returning the perimeter of the rectangle.
+
+If one of the arguments is negative it should return -1.
+Expected function*/
+func RectPerimeter(w, h int) int {
+
+}
