46124b893ca751717ddeabce526f4e727093c009
luccianox <kevinnambubbi@gmail.com>
2025-09-25 08:41:55 +0300

    printmemory

--- printmemory.go
+++ printmemory.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*Instructions
 Write a function that takes (arr [10]byte), and displays the memory as in the example.
 
@@ -9,7 +8,6 @@ The ASCII graphic characters are any characters intended to be written, printed,
 or otherwise displayed in a form that can be read by humans, present on the ASCII encoding.
 Expected function*/
 
-=======
 /*
 printmemory
 Instructions
@@ -22,77 +20,50 @@ The ASCII graphic characters are any characters intended to be written, printed,
 present on the ASCII encoding.
 Expected function
 */
->>>>>>> ffbb1941cbe80c2c3a716c29c8ce1bf9c71b5be0
+
 package main
 
 import "github.com/01-edu/z01"
 
 func PrintMemory(arr [10]byte) {
-<<<<<<< HEAD
 
-	//loop through arr
-	for i := 0; i <= arr; i++ {
+	for i := 0; i < len(arr); i++ {
 		high := arr[i] / 16
 		low := arr[i] % 16
 		//print high
-=======
-	// loop through arr
-
-	for i := 0; i < len(arr); i++ {
-
-		high := arr[i] / 16
-
-		low := arr[i] % 16
-
-		// print high
->>>>>>> ffbb1941cbe80c2c3a716c29c8ce1bf9c71b5be0
-
 		if high < 10 {
+
 			z01.PrintRune(rune('0' + high))
 		} else {
 			z01.PrintRune(rune('a' + (high - 10)))
 		}
+		//print low
+
 		if low < 10 {
 			z01.PrintRune(rune('0' + low))
 		} else {
 			z01.PrintRune(rune('a' + (low - 10)))
 		}
+		z01.PrintRune(' ')
 		if (i+1)%4 == 0 {
-<<<<<<< HEAD
 
-			z01.PrintRune(' ')
-		} else {
 			z01.PrintRune('\n')
 		}
 	}
-	//Print ASCII characters
-
+	if len(arr)%4 != 0 {
+		z01.PrintRune('\n')
+	}
+	//Print ASCII
 	for _, ch := range arr {
 
 		if ch >= 32 && ch <= 126 {
 			z01.PrintRune(rune(ch))
-=======
-			z01.PrintRune('\n')
-		} else {
-			z01.PrintRune(' ')
-		}
-	}
-	z01.PrintRune('\n')
-	// print ASCII
-	for i := 0; i < len(arr); i++ {
-		if arr[i] >= 32 && arr[i] <= 126 {
-			z01.PrintRune(rune(arr[i]))
->>>>>>> ffbb1941cbe80c2c3a716c29c8ce1bf9c71b5be0
 		} else {
 			z01.PrintRune('.')
 		}
 	}
 	z01.PrintRune('\n')
 }
-<<<<<<< HEAD
-=======
-
 func main() {
 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
 }
->>>>>>> ffbb1941cbe80c2c3a716c29c8ce1bf9c71b5be0
