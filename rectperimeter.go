723c31c2246c9ffd55052da73d785b0df9a2534c
luccianox <kevinnambubbi@gmail.com>
2025-09-25 08:50:59 +0300

    rectperimeter.go

--- rectperimeter.go
+++ rectperimeter.go
@@ -1,10 +1,26 @@
-/*rectperimeter
+/*
+rectperimeter
 Instructions
 Write a function that takes two int's as arguments, representing the
 length of width and height of a rectangle and returning the perimeter of the rectangle.
 
 If one of the arguments is negative it should return -1.
-Expected function*/
+Expected function
+*/
+package main
+
+import "fmt"
+
 func RectPerimeter(w, h int) int {
 
+	if w < 0 || h < 0 {
+
+		return -1
+	}
+	return 2 * (w + h)
+}
+func main() {
+	fmt.Println(RectPerimeter(10, 2))
+	fmt.Println(RectPerimeter(434343, 898989))
+	fmt.Println(RectPerimeter(10, -2))
 }
