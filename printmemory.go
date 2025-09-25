110f794688d871c5c2458053923abf00327a9c5d
luccianox <kevinnambubbi@gmail.com>
2025-09-25 07:36:31 +0300

    printmemory

---
+++ printmemory.go
@@ -0,0 +1,51 @@
+/*Instructions
+Write a function that takes (arr [10]byte), and displays the memory as in the example.
+
+After displaying the memory the function must display all the ASCII graphic characters.
+The non printable characters must be replaced by a dot.
+
+The ASCII graphic characters are any characters intended to be written, printed,
+or otherwise displayed in a form that can be read by humans, present on the ASCII encoding.
+Expected function*/
+
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintMemory(arr [10]byte) {
+
+	//loop through arr
+	for i := 0; i <= arr; i++ {
+		high := arr[i] / 16
+		low := arr[i] % 16
+		//print high
+
+		if high < 10 {
+			z01.PrintRune(rune('0' + high))
+		} else {
+			z01.PrintRune(rune('a' + (high - 10)))
+		}
+		if low < 10 {
+			z01.PrintRune(rune('0' + low))
+		} else {
+			z01.PrintRune(rune('a' + (low - 10)))
+		}
+		if (i+1)%4 == 0 {
+
+			z01.PrintRune(' ')
+		} else {
+			z01.PrintRune('\n')
+		}
+	}
+	//Print ASCII characters
+
+	for _, ch := range arr {
+
+		if ch >= 32 && ch <= 126 {
+			z01.PrintRune(rune(ch))
+		} else {
+			z01.PrintRune('.')
+		}
+	}
+	z01.PrintRune('\n')
+}
