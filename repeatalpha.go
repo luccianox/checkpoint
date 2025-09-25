1a5b91211c5774891afe7b5fb5633222c0f8f08a
luccianox <kevinnambubbi@gmail.com>
2025-09-25 09:54:34 +0300

    repeatalpha

--- repeatalpha.go
+++ repeatalpha.go
@@ -1,31 +1,48 @@
-/*repeatalpha
+/*
+repeatalpha
 Instructions
 
-Write a function called RepeatAlpha that takes a string and displays it repeating each alphabetical character as many times as its alphabetical index.
+Write a function called RepeatAlpha that takes a string and displays it repeating each alphabetica
+l character as many times as its alphabetical index.
 
 'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
-Expected Function*/
+Expected Function
+*/
+package main
+
+import "fmt"
 
 func RepeatAlpha(s string) string {
-	result := []rune{}
-	for _, ch := range s{
 
-		if ch >= 'a' && ch <= 'z'{
-			index := ch - 'a' + 1
+	//loop through s
+	result := ""
+	for i := 0; i < len(s); i++ {
+		//look for small letters
+		char := s[i]
+		if s[i] >= 'a' && s[i] <= 'z' {
 
-			for i := 0, i <int(len(index)); i++{
+			index := char - 'a' + 1
+			for j := 0; j < int((index)); j++ {
 
-				result = append(result, ch)
+				result += string(char)
 			}
-		}
-	}else if ch >= 'A' && ch <= 'Z'{
-		index := ch - 'A' + 1
-		for i := 0; i < int(len(index)); i++{
+		} else if s[i] >= 'A' && s[i] <= 'Z' {
 
-			result = append(result, ch)
+			index := char - 'A' + 1
+			for j := 0; j < int((index)); j++ {
+
+				result += string(char)
+			}
+		} else {
+			result += string(char)
 		}
-	}else{
-		result = (append(result, ch))
+
 	}
-	return string(result)
+	return result
+}
+func main() {
+	fmt.Println(RepeatAlpha("abc"))
+	fmt.Println(RepeatAlpha("Choumi."))
+	fmt.Println(RepeatAlpha(""))
+	fmt.Println(RepeatAlpha("abacadaba 01!"))
 }
